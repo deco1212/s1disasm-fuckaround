d2625bce502085d5946ec05bfaafdae3b807967d
DecoGenus <andreethangamer@gmail.com>
2025-12-28 22:45:22 -0300

    GHZ CHANGES

--- SampleTable.asm
+++ SampleTable.asm
@@ -5,8 +5,8 @@ SampleTable:
 	dcSample	TYPE_DPCM, 		Kick, 		8000				; $81
 	dcSample	TYPE_PCM,		Snare,		24000				; $82
 	dcSample	TYPE_DPCM, 		Timpani, 	7250				; $83
-	dcSample    TYPE_PCM,       Fairy,      0, FLAGS_SFX        ; $84
-	dcSample    TYPE_PCM,       Hiroshima,  0, FLAGS_SFX        ; $85
+	dcSample	TYPE_NONE										; $84
+	dcSample	TYPE_NONE										; $85
 	dcSample	TYPE_NONE										; $86
 	dcSample	TYPE_NONE										; $87
 	dcSample	TYPE_DPCM, 		Timpani, 	9750				; $88
@@ -20,7 +20,5 @@ SampleTable:
 	incdac	Kick, "sound/dac/kick.dpcm"
 	incdac	Snare, "sound/dac/snare.pcm"
 	incdac	Timpani, "sound/dac/timpani.dpcm"
-	incdac  Fairy, "sound/dac/fairy.wav"
-	incdac  Hiroshima, "sound/dac/hiroshima.wav"
 	incdac	SegaPCM, "sound/dac/sega.wav"
 	even
